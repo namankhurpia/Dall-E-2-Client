0da84f9ee36cf094f1b6515eab1803be83c3d294
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2023-03-18 11:57:11 -0400

    adding changes

--- app/src/main/java/com/namankhurpia/openaiapi/AboutUs.java
+++ app/src/main/java/com/namankhurpia/openaiapi/AboutUs.java
@@ -3,12 +3,15 @@ package com.namankhurpia.openaiapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class AboutUs extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_about_us);
     }
 }
